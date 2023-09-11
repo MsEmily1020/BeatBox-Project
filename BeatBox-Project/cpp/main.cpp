86156a1c18b9140f12511cda207fb830aef392a1
MsEmily1020 <peace10200khs@gmail.com>
2023-09-12 08:59:03 +0900

    💄 음악 소스 추가

--- BeatBox-Project/cpp/main.cpp
+++ BeatBox-Project/cpp/main.cpp
@@ -5,9 +5,19 @@ using namespace sf;
 const int WIDTH = 1200;
 const int HEIGHT = 1000;
 
+class Object {
+public:
+	int x_;
+	int y_;
+	RectangleShape sprite_;
+};
+
 int main() {
 	RenderWindow  window(VideoMode(WIDTH, HEIGHT), "MusicTokTok");
 	window.setFramerateLimit(15);
+
+	Object player;
+
 
 	while (window.isOpen())
 	{
