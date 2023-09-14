2cca6895315c25cb98fe7c5ceec91590140a7235
MsEmily1020 <peace10200khs@gmail.com>
2023-09-14 10:10:19 +0900

    ✨ 게임시작 및 게임설명 버튼 생성

--- BeatBox-Project/cpp/main.cpp
+++ BeatBox-Project/cpp/main.cpp
@@ -1,29 +1,45 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
+using namespace std;
 using namespace sf;
 
-const int WIDTH = 1200;
-const int HEIGHT = 1000;
+const int WIDTH = 900;
+const int HEIGHT = 600;
 
-class Object {
+class Button {
 public:
-	int x_;
-	int y_;
+	int width_;
+	int height_;
 	RectangleShape sprite_;
+	Vector2i mouse_pos;
+
+	void clickBtn() {
+		if (sprite_.getGlobalBounds().contains(mouse_pos.x, mouse_pos.y)) {
+			if (Mouse::isButtonPressed(Mouse::Left)) {
+				cout << "1" << endl;
+			}
+		}
+	}
 };
 
 int main() {
 	RenderWindow  window(VideoMode(WIDTH, HEIGHT), "MusicTokTok");
 	window.setFramerateLimit(15);
-	
+
 	Texture texture;
-	if (!texture.loadFromFile("image/player.png"))
+	if (!texture.loadFromFile("image/startbtn.png"))
 		return -1;
+
+	Button startBtn;
+	startBtn.sprite_.setPosition(130, 370);
+	startBtn.sprite_.setSize(Vector2f(300, 250));
+	startBtn.sprite_.setTexture(&texture);
 	
-	Object player;
-	player.sprite_.setPosition(200, 300);
-	player.sprite_.setSize(Vector2f(100, 100));
-	player.sprite_.setTexture(&texture);
+	Button explanBtn;
+	explanBtn.sprite_.setFillColor(Color::Blue);
+	explanBtn.sprite_.setPosition(550, 450);
+	explanBtn.sprite_.setSize(Vector2f(200, 80));
 
 	while (window.isOpen())
 	{
@@ -32,13 +48,19 @@ int main() {
 		{
 			if (e.type == Event::Closed)
 				window.close();
+
+			startBtn.mouse_pos = Mouse::getPosition(window);
+			explanBtn.mouse_pos = Mouse::getPosition(window);
+
+			startBtn.clickBtn();
+			explanBtn.clickBtn();
 		}
 
-
 		window.clear();
-		
-		window.draw(player.sprite_);
-		
+
+		window.draw(startBtn.sprite_);
+		window.draw(explanBtn.sprite_);
+
 		window.display();
 	}
 	return 0;
---
+++ BeatBox-Project/newjeans.cpp
@@ -0,0 +1,44 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+const int WIDTH = 1200;
+const int HEIGHT = 1000;
+
+class Object {
+public:
+	int x_;
+	int y_;
+	RectangleShape sprite_;
+};
+
+int main() {
+	RenderWindow  window(VideoMode(WIDTH, HEIGHT), "MusicTokTok");
+	window.setFramerateLimit(15);
+
+	Texture texture;
+	if (!texture.loadFromFile("image/player.png"))
+		return -1;
+
+	Object player;
+	player.sprite_.setPosition(500, 600);
+	player.sprite_.setSize(Vector2f(100, 100));
+	player.sprite_.setTexture(&texture);
+
+	while (window.isOpen())
+	{
+		Event e;
+		while (window.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+
+		window.draw(player.sprite_);
+
+		window.display();
+	}
+	return 0;
+}
